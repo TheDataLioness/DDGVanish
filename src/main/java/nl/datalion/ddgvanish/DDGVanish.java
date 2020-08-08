cb7af23416ad58befc632684ffc3eb71fea501a7
DataLiontje <datalionmcpe@gmail.com>
2020-08-08 22:19:39 +0200

    Removed useless imports

--- src/main/java/nl/datalion/ddgvanish/DDGVanish.java
+++ src/main/java/nl/datalion/ddgvanish/DDGVanish.java
@@ -3,19 +3,7 @@ package nl.datalion.ddgvanish;
 import nl.datalion.ddgvanish.commands.VanishCommand;
 import nl.datalion.ddgvanish.utils.Vanish;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
 
 public final class DDGVanish extends JavaPlugin {
 
